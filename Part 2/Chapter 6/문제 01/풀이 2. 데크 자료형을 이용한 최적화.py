9f5e60b664275da9e789ff78aff14535755bced4
LimKwangyoung <kokoz22@naver.com>
2023-05-10 17:07:27 +0900

    Part 2 - Chapter 6

---
+++ Part 2/Chapter 6/문제 01/풀이 2. 데크 자료형을 이용한 최적화.py
@@ -0,0 +1,22 @@
+import collections
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # 자료형 데크로 선언
+        strs = collections.deque()
+
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+
+        while len(strs) > 1:
+            if strs.popleft() != strs.pop():
+                return False
+
+        return True
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.isPalindrome(input()))
---
+++ Part 2/Chapter 6/문제 01/풀이.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        character_lst = []
+        for i in s:
+            if i.isalnum():
+                character_lst.append(i)
+
+        left, right = 0, len(character_lst) - 1
+        while left < right:
+            if character_lst[left].lower() != character_lst[right].lower():
+                return False
+            left += 1
+            right -= 1
+        else:
+            return True
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.isPalindrome(input()))
