5c7cbe2a57eb58321b79bef014804be8388adb50
kokoz22 <kokoz22@naver.com>
2023-10-11 22:46:43 +0900

    Part 4 - Chapter 12

--- Part 4/Chapter 12/문제 36/풀이 0.py
+++ Part 4/Chapter 12/문제 36/풀이 0.py
@@ -1,11 +1,17 @@
 class Solution:
     def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
-        def dfs(lst: list[int], t: int) -> None:
-            if
+        def dfs(lst: list[int], idx: int, t: int) -> None:
+            if sum(lst) == t:
+                result.append(lst)
 
-            for i in lst:
-                dfs(lst, t - i)
-
+            for i in range(idx, len(candidates)):
+                elements = lst[:]
+                for j in range((target - sum(lst)) // candidates[i]):
+                    elements.append(candidates[i])
+                    dfs(elements, i + 1, target - sum(elements))
+        result = []
+        dfs([], 0, target)
+        return result
 
 
 if __name__ == '__main__':
