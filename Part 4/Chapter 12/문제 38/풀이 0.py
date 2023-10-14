16985838fa9faba03f02d5c7b2efab4cd029363d
kokoz22 <kokoz22@naver.com>
2023-10-15 01:09:09 +0900

    Part 4 - Chapter 12

--- Part 4/Chapter 12/문제 38/풀이 0.py
+++ Part 4/Chapter 12/문제 38/풀이 0.py
@@ -8,19 +8,41 @@ class Solution:
             airlines[i].append(j)
         for i in airlines:
             airlines[i].sort(reverse=True)
-        return airlines
+        # return airlines
 
-        result = []
-        airport = 'JFK'
+        path = ['JFK']
+        destination = ''
         while True:
-            result.append(airport)
+            airport = path[-1]
             if not airlines[airport]:
-                return result
-            airport = airlines[airport].pop()
+                break
+            elif airlines[airlines[airport][-1]]:
+                path.append(airlines[airport].pop())
+            else:
+                destination = airlines[airport].pop()
+
+# import collections
+# class Solution:
+#     def findItinerary(self, tickets: list[list[str]]) -> list[str]:
+#         graph = collections.defaultdict(list)
+#         tickets.sort(key = lambda x : x[1], reverse = True)
+#         for src, dst in tickets:
+#           graph[src].append(dst)
+#
+#         res, stack = [], ['JFK']
+#         while stack:
+#           if stack[-1] in graph and graph[stack[-1]]:
+#             stack.append(graph[stack[-1]].pop())
+#
+#           else:
+#             res.append(stack.pop())
+#
+#         return res[::-1]
+
 
 
 if __name__ == '__main__':
     solution = Solution()
     # print(solution.findItinerary([["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]))
     # print(solution.findItinerary([["JFK", "SFO"], ["JFK", "ATL"], ["SFO", "ATL"], ["ATL", "JFK"], ["ATL", "SFO"]]))
-    print(solution.findItinerary([["JFK", "KUL"], ["JFK", "NRT"], ["NRT", "JFK"]]))
+    print(solution.findItinerary([["JFK","KUL"],["JFK","NRT"],["NRT","JFK"]]))
