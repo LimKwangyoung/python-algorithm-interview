2102b0727abcf69c6fcc226a8c0bffee07f58b59
kokoz22 <kokoz22@naver.com>
2024-01-06 23:50:29 +0900

    Part 4 - Chapter 13

--- Part 4/Chapter 13/문제 40/풀이 0.py
+++ Part 4/Chapter 13/문제 40/풀이 0.py
@@ -7,21 +7,26 @@ class Solution:
         for u, v, w in times:
             nodes[u].append([v, w])
 
-        cnt = 0
         que = collections.deque([k])
         visited = [False] * (n + 1)
-        visited[k] = True
+        visited[k] = 0
         while que:
-            vertex = que.popleft()
-            for i in range(len(nodes[vertex])):
-                if nodes[vertex][i][1] >= 1:
-                    nodes[vertex][i][1] -= 1
-                    if nodes[vertex][i][1] == 0:
-                        que.append(nodes[vertex][i][0])
-            cnt += 1
-        return cnt
+            u = que.popleft()
+            for v, w in nodes[u]:
+                if v != k:
+                    if visited[v]:
+                        visited[v] = min(visited[v], visited[u] + w)
+                    else:
+                        visited[v] = visited[u] + w
+                    que.append(v)
+
+        for i in visited[1:]:
+            if i is False:
+                return -1
+        else:
+            return max(visited[1:])
 
 
 if __name__ == '__main__':
     solution = Solution()
-    print(solution.networkDelayTime(times=[[2, 1, 1], [2, 3, 1], [3, 4, 1]], n=4, k=2))
+    print(solution.networkDelayTime(times=[[1, 2, 1], [2, 1, 3]], n=2, k=2))
