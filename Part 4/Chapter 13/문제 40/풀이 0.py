cb0c54ff178fc979955c8fb498c3903a1958a680
kokoz22 <kokoz22@naver.com>
2024-01-07 14:49:16 +0900

    Part 4 - Chapter 13

--- Part 4/Chapter 13/문제 40/풀이 0.py
+++ Part 4/Chapter 13/문제 40/풀이 0.py
@@ -1,32 +1,31 @@
 import collections
+import sys
 
 
 class Solution:
     def networkDelayTime(self, times: list[list[int]], n: int, k: int) -> int:
-        nodes = collections.defaultdict(list)
+        graph = collections.defaultdict(list)
         for u, v, w in times:
-            nodes[u].append([v, w])
+            graph[u].append((v, w))
 
         que = collections.deque([k])
-        visited = [False] * (n + 1)
+        visited = [None] * (n + 1)
         visited[k] = 0
         while que:
             u = que.popleft()
-            for v, w in nodes[u]:
-                if v != k:
-                    if visited[v]:
-                        visited[v] = min(visited[v], visited[u] + w)
-                    else:
-                        visited[v] = visited[u] + w
+            for v, w in graph[u]:
+                if visited[v] is None or visited[u] + w < visited[v]:
+                    visited[v] = visited[u] + w
                     que.append(v)
 
+        result = -sys.maxsize
         for i in visited[1:]:
-            if i is False:
+            if i is None:
                 return -1
-        else:
-            return max(visited[1:])
+            result = max(result, i)
+        return result
 
 
 if __name__ == '__main__':
     solution = Solution()
-    print(solution.networkDelayTime(times=[[1, 2, 1], [2, 1, 3]], n=2, k=2))
+    print(solution.networkDelayTime(times=[[2, 1, 1], [2, 3, 1], [3, 4, 1]], n=4, k=2))
---
+++ Part 4/Chapter 13/문제 40/풀이 1. 다익스트라 알고리즘 구현.py
@@ -0,0 +1,33 @@
+import collections
+import heapq
+
+
+class Solution:
+    def networkDelayTime(self, times: list[list[int]], n: int, k: int) -> int:
+        graph = collections.defaultdict(list)
+        # 그래프 인접 리스트 구성
+        for u, v, w in times:
+            graph[u].append((v, w))
+
+        # 큐 변수: [(소요 시간, 정점)]
+        Q = [(0, k)]
+        dist = collections.defaultdict(int)
+
+        # 우선순위 큐 최솟값 기준으로 정점까지 최단 경로 삽입
+        while Q:
+            time, node = heapq.heappop(Q)
+            if node not in dist:
+                dist[node] = time
+                for v, w in graph[node]:
+                    alt = time + w
+                    heapq.heappush(Q, (alt, v))
+
+        # 모든 노드의 최단 경로 존재 여부 판별
+        if len(dist) == n:
+            return max(dist.values())
+        return -1
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.networkDelayTime(times=[[2, 1, 1], [2, 3, 1], [3, 4, 1]], n=4, k=2))
