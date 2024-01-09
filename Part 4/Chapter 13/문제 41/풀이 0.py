e2ee67b3148624786b2df2d70bfe91fe7fb63234
kokoz22 <kokoz22@naver.com>
2024-01-09 22:03:42 +0900

    Part 4 - Chapter 13

--- Part 4/Chapter 13/문제 41/풀이 0.py
+++ Part 4/Chapter 13/문제 41/풀이 0.py
@@ -1,5 +1,4 @@
 import collections
-import heapq
 
 
 class Solution:
@@ -8,21 +7,22 @@ class Solution:
         for start, end, weight in flights:
             graph[start][end] = weight
 
-        que = [(0, src, 0)]  # (distance, vertex, stopover)
-        visited = collections.defaultdict(list)
-        while que:
-            distance, start, stopover = heapq.heappop(que)
-            if stopover <= k + 1:
-                # visited[start][distance] = stopover
-                visited[start].append(distance)
-                for end in graph[start]:
-                    heapq.heappush(que, (distance + graph[start][end], end, stopover + 1))
-        # print(visited)
-        return min(visited[dst]) if dst in visited else -1
-        # return visited[dst] if dst in visited else -1
+        que = collections.deque([(src, 0)])  # (vertex, distance)
+        visited = [None] * n
+        visited[src] = 0
+        for _ in range(k + 1):
+            temp = collections.deque([])
+            while que:
+                vertex, distance = que.popleft()
+                for v in graph[vertex]:
+                    if not visited[v] or distance + graph[vertex][v] < visited[v]:
+                        visited[v] = distance + graph[vertex][v]
+                        temp.append((v, distance + graph[vertex][v]))
+            que = temp
+
+        return visited[dst] if visited[dst] else -1
 
 
 if __name__ == '__main__':
     solution = Solution()
-    # print(solution.findCheapestPrice(n=3, flights=[[0, 1, 100], [1, 2, 100], [0, 2, 500]], src=0, dst=2, k=0))
-    print(solution.findCheapestPrice(n=3, flights=[[0,1,100],[1,2,100],[0,2,500]], src=0, dst=2, k=1))
+    print(solution.findCheapestPrice(n=3, flights=[[0, 1, 100], [1, 2, 100], [0, 2, 500]], src=0, dst=2, k=0))
